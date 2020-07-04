41bb46d03b7686f9647dbec693b0c897abc9f776
advaithm <advaith.madhukar@gmail.com>
2020-07-04 12:51:53 +0530

    login

--- home/views.py
+++ home/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import  authenticate
 # Create your views here.
 def home(request):
-    response = render(request, "home/home.html")
-    return response
+    if request.method=="POST":
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        user = authenticate(username=username, password=passwd)
+        if user:
+            redirect('downloads')
+            
+    else:
+        response = render(request, "home/home.html")
+        return response
